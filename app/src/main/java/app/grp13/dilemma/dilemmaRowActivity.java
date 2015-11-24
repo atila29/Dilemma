c0a56dc4c27923c4aa1663c67073cf065a14e1c2
nicolaihansen <nicolaihansen@live.dk>
2015-11-24 22:52:31 +0100

    Lavet custom rows i liste

---
+++ app/src/main/java/app/grp13/dilemma/dilemmaRowActivity.java
@@ -0,0 +1,13 @@
+package app.grp13.dilemma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class dilemmaRowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dilemma_row);
+    }
+}
