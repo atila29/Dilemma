250a677db61049b5c43995636194c1f27ba607fb
nicolaihansen <nicolaihansen@live.dk>
2015-11-25 02:53:30 +0100

    Fixet lidt i create dilemma

--- app/src/main/java/app/grp13/dilemma/CreateDilemma.java
+++ app/src/main/java/app/grp13/dilemma/CreateDilemma.java
@@ -5,17 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.NumberPicker;
 import android.widget.TextView;
 
-import com.wefika.horizontalpicker.HorizontalPicker;
-
-import org.w3c.dom.Text;
-
 public class CreateDilemma extends AppCompatActivity implements View.OnClickListener {
-    HorizontalPicker gravityPicker;
-    HorizontalPicker answerCount;
-    Button setCountButton;
     Button createDilemma;
     TextView answerText;
     EditText answer1;
@@ -23,64 +15,96 @@ public class CreateDilemma extends AppCompatActivity implements View.OnClickList
     EditText answer3;
     EditText answer4;
     EditText answer5;
+    Button gravity1Btn, gravity2Btn, gravity3Btn, gravity4Btn, gravity5Btn;
+    Button selectedButton;
 
     String[] gravity = {"1", "2", "3", "4", "5"};
-    String[] ACount = {"2", "3", "4", "5"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_dilemma);
-        gravityPicker = (HorizontalPicker) findViewById(R.id.gravityPicker);
-        answerCount = (HorizontalPicker) findViewById(R.id.answerCount);
-        setCountButton = (Button) findViewById(R.id.setCountButton);
-        createDilemma = (Button) findViewById(R.id.createDilButton);
-        answerText = (TextView) findViewById(R.id.answerText);
+
         answer1 = (EditText) findViewById(R.id.answer1);
         answer2 = (EditText) findViewById(R.id.answer2);
         answer3 = (EditText) findViewById(R.id.answer3);
         answer4 = (EditText) findViewById(R.id.answer4);
         answer5 = (EditText) findViewById(R.id.answer5);
-        answerCount.setValues(ACount);
-        answerCount.setSelectedItem(2);
-        gravityPicker.setValues(gravity);
-        gravityPicker.setSelectedItem(2);
-        setCountButton.setOnClickListener(this);
+        gravity1Btn = (Button) findViewById(R.id.gravity1Btn);
+        gravity2Btn = (Button) findViewById(R.id.gravity2Btn);
+        gravity3Btn = (Button) findViewById(R.id.gravity3Btn);
+        gravity4Btn = (Button) findViewById(R.id.gravity4Btn);
+        gravity5Btn = (Button) findViewById(R.id.gravity5Btn);
+        gravity1Btn.setOnClickListener(this);
+        gravity2Btn.setOnClickListener(this);
+        gravity3Btn.setOnClickListener(this);
+        gravity4Btn.setOnClickListener(this);
+        gravity5Btn.setOnClickListener(this);
+
+        answer1.setVisibility(View.VISIBLE);
+        answer2.setVisibility(View.VISIBLE);
+        answer3.setVisibility(View.INVISIBLE);
+        answer4.setVisibility(View.INVISIBLE);
+        answer5.setVisibility(View.INVISIBLE);
+
+        selectedButton = gravity1Btn;
 
 
-
+        answer1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus && !answer1.getText().equals("")) {
+                    answer3.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        answer2.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus && !answer2.getText().equals("")) {
+                    answer4.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        answer3.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus && !answer3.getText().equals("")) {
+                    answer5.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
-        if(v == setCountButton){
-            int tempCount = answerCount.getSelectedItem()+2;
-            answerCount.setVisibility(View.GONE);
-            setCountButton.setVisibility(View.GONE);
-            answerText.setText("Svarmuligheder: ");
-            createDilemma.setVisibility(View.VISIBLE);
-            if(tempCount==2){
-                answer1.setVisibility(View.VISIBLE);
-                answer2.setVisibility(View.VISIBLE);
-            }
-            else if(tempCount==3){
-                answer1.setVisibility(View.VISIBLE);
-                answer2.setVisibility(View.VISIBLE);
-                answer3.setVisibility(View.VISIBLE);
-            }
-            else if(tempCount==4){
-                answer1.setVisibility(View.VISIBLE);
-                answer2.setVisibility(View.VISIBLE);
-                answer3.setVisibility(View.VISIBLE);
-                answer4.setVisibility(View.VISIBLE);
-            }
-            else if(tempCount==5){
-                answer1.setVisibility(View.VISIBLE);
-                answer2.setVisibility(View.VISIBLE);
-                answer3.setVisibility(View.VISIBLE);
-                answer4.setVisibility(View.VISIBLE);
-                answer5.setVisibility(View.VISIBLE);
-            }
+        if (v == gravity1Btn) {
+            gravity1Btn.setBackgroundResource(R.drawable.gravity1_btn_selected);
+            gravity2Btn.setBackgroundResource(R.drawable.gravity2_button);
+            gravity3Btn.setBackgroundResource(R.drawable.gravity3_button);
+            gravity4Btn.setBackgroundResource(R.drawable.gravity4_button);
+            gravity5Btn.setBackgroundResource(R.drawable.gravity5_button);
+        } else if (v == gravity2Btn) {
+            gravity1Btn.setBackgroundResource(R.drawable.gravity1_button);
+            gravity2Btn.setBackgroundResource(R.drawable.gravity2_btn_selected);
+            gravity3Btn.setBackgroundResource(R.drawable.gravity3_button);
+            gravity4Btn.setBackgroundResource(R.drawable.gravity4_button);
+            gravity5Btn.setBackgroundResource(R.drawable.gravity5_button);
+        } else if (v == gravity3Btn) {
+            gravity1Btn.setBackgroundResource(R.drawable.gravity1_button);
+            gravity2Btn.setBackgroundResource(R.drawable.gravity2_button);
+            gravity3Btn.setBackgroundResource(R.drawable.gravity3_btn_selected);
+            gravity4Btn.setBackgroundResource(R.drawable.gravity4_button);
+            gravity5Btn.setBackgroundResource(R.drawable.gravity5_button);
+        } else if (v == gravity4Btn) {
+            gravity1Btn.setBackgroundResource(R.drawable.gravity1_button);
+            gravity2Btn.setBackgroundResource(R.drawable.gravity2_button);
+            gravity3Btn.setBackgroundResource(R.drawable.gravity3_button);
+            gravity4Btn.setBackgroundResource(R.drawable.gravity4_btn_selected);
+            gravity5Btn.setBackgroundResource(R.drawable.gravity5_button);
+        } else if (v == gravity5Btn) {
+            gravity1Btn.setBackgroundResource(R.drawable.gravity1_button);
+            gravity2Btn.setBackgroundResource(R.drawable.gravity2_button);
+            gravity3Btn.setBackgroundResource(R.drawable.gravity3_button);
+            gravity4Btn.setBackgroundResource(R.drawable.gravity4_button);
+            gravity5Btn.setBackgroundResource(R.drawable.gravity5_btn_selected);
         }
     }
 }
